6c600c6ec1cfcd603f0f7b12f61f500a5c3a5ae8
unamednada <dias.gustavo.dias@gmail.com>
2021-11-03 15:18:49 -0300

    Create name in state using hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Greeting() {
   // substitua essa variável por uma variável no estado, utilizando useState
-  const name = '';
+  const [name, setName] = useState('');
 
   function handleChange(event) {
     // atualize o valor do estado com base no que está em event.target.value
